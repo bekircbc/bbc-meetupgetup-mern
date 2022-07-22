fd642ca8d3c82b6003d5a85b684faa1e6abe2059
Bekir - CDI <b.burakcebecii@gmail.com>
2022-07-22 12:20:57 +0200

    login page is ready without post and login check.. next step is register page..

--- src/components/PageLogin.jsx
+++ src/components/PageLogin.jsx
@@ -1,56 +1,42 @@
-// import { useRef } from "react";
+import { useRef } from "react";
 
 export function PageLogin() {
-  // const titleInputRef = useRef();
-  // const imageInputRef = useRef();
-  // const addressInputRef = useRef();
-  // const descriptionInputRef = useRef();
-  // function submitHandler(event) {
-  //   event.preventDefault();
-  //   const enteredTitle = titleInputRef.current.value;
-  //   const enteredImage = imageInputRef.current.value;
-  //   const enteredAddress = addressInputRef.current.value;
-  //   const enteredDescription = descriptionInputRef.current.value;
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
+  function submitHandler(event) {
+    event.preventDefault();
+    const enteredUsername = usernameInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
 
-  //   const meetupData = {
-  //     title: enteredTitle,
-  //     image: enteredImage,
-  //     address: enteredAddress,
-  //     description: enteredDescription,
-  //   };
+    const meetupData = {
+      username: enteredUsername,
+      password: enteredPassword,
+    };
 
-  //   fetch(
-  //     "https://basic-streaming-app-default-rtdb.firebaseio.com/meetups.json",
-  //     {
-  //       method: "POST",
-  //       body: JSON.stringify(meetupData),
-  //       headers: { "Content-Type": "application/json" },
-  //     }
-  //   );
-  // }
+    return meetupData;
+
+    //   fetch(
+    //     "https://basic-streaming-app-default-rtdb.firebaseio.com/meetups.json",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify(meetupData),
+    //       headers: { "Content-Type": "application/json" },
+    //     }
+    //   );
+  }
+
   return (
     <section className="LoginPage">
       <h1>Login</h1>
-      {/* <form className="formCard" onSubmit={submitHandler}>
-        <label htmlFor="username">Meetup Title</label>
-        <input type="text" required id="username" ref={titleInputRef} />
+      <form className="formCard" onSubmit={submitHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" required id="username" ref={usernameInputRef} />
 
-        <label htmlFor="image">Meetup Image</label>
-        <input type="url" required id="image" ref={imageInputRef} />
+        <label htmlFor="password">Password</label>
+        <input type="password" required id="password" ref={passwordInputRef} />
 
-        <label htmlFor="address">Address</label>
-        <input type="text" required id="address" ref={addressInputRef} />
-
-        <label htmlFor="description">Meetup Description</label>
-        <textarea
-          id="description"
-          required
-          rows="5"
-          ref={descriptionInputRef}
-        ></textarea>
-
-        <button className="btnAddMeetup">Add Meetup</button>
-      </form> */}
+        <button className="btnLogin">Login</button>
+      </form>
     </section>
   );
 }
